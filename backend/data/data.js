09af81d9b22be6497a5dbd3c399666c8dffd7671
Ravi kant <183037@nith.ac.in>
2024-05-20 14:51:04 +0530

    backend setup with dummy data

---
+++ backend/data/data.js
@@ -0,0 +1,37 @@
+ const chats =[
+    {
+        isGroupChat: false,
+        users:[
+            {
+                name:"ravi kant",
+                email:"183037@nith.ac.in"
+            },
+            {
+                name:"shivda sharma",
+                email:"183045@nith.ac.in"
+            }
+        ],
+        _id:"12321312312",
+        chatName: "J1"
+    
+    },
+
+    {
+        isGroupChat: false,
+        users:[
+            {
+                name:"Arun",
+                email:"183008@nith.ac.in"
+            },
+            {
+                name:"Hrikshant",
+                email:"183004@nith.ac.in"
+            }
+        ],
+        _id:"55665656",
+        chatName: "J2"
+    
+    }
+
+]
+module.exports={chats};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
 
 app.get('/',(req,res)=>{
     res.send("hii in node")
+});
+
+app.get('/chats',(req,res)=>{
+    res.status(200).send(chats);
+});
+
+app.get('/chats/:id',(req,res)=>{
+    console.log(req.params.id);
+    const chat=chats.find(c=>c._id===req.params.id);
+    res.send(chat);
 })
-app.listen(8000,()=>{
-    console.log("server is up and running");
+app.listen(PORT,()=>{
+    console.log(`server is up and running ${PORT}`);
 });
