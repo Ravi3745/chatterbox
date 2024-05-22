2e72f89423303727b27d01f268fa3e91a9a913d0
Ravi kant <183037@nith.ac.in>
2024-05-22 19:03:59 +0530

    login and signup both working-frontend

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,11 +1,46 @@
 // import React from 'react'
 
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useState } from "react";
-
-
+import Spinner from '../../utilities/Spinner';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 const Login = () => {
 
   const [loginData,setLoginData] = useState({email:"",password:""});
+  const [showPassword,setShowPassword]= useState(false);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+  function togglePasswordVisibility(){
+    setShowPassword(!showPassword);
+  }
+
+  async function handleSubmit  (e){
+    e.preventDefault();
+    console.log("hloo1")
+    setLoading(true);
+   const {email,password} = loginData;
+    try {
+      const config ={
+        headers:{
+          "Content-type":"application/json"
+        }
+      };
+      console.log("hloo2")
+      const { data } = await axios.post('http://localhost:8000/user/login',{email,password},config);
+      toast("Login Successful");
+      localStorage.setItem("userInfo",JSON.stringify(data));
+      setLoading(false);
+      history.push('/chats');
+
+    } catch (error) {
+      toast("Something went wrong");
+      setLoading(false);
+    }
+
+  }
+
 
 
   return (
@@ -18,7 +53,7 @@ const Login = () => {
     </div>
 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form className="space-y-6" action="#" method="POST">
+      <form className="space-y-6" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
             Email address
@@ -32,7 +67,7 @@ const Login = () => {
               onChange={e=>setLoginData({...loginData,email:e.target.value})}
               autoComplete="email"
               required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
             />
           </div>
         </div>
@@ -48,18 +83,24 @@ const Login = () => {
               </a>
             </div>
           </div>
+         
           <div className="mt-2">
             <input
               id="password"
               name="password"
-              type="password"
+              type={showPassword?"text":"password"}
               autoComplete="current-password"
               required
               value={loginData.password}
               onChange={e=>setLoginData({...loginData,password:e.target.value})}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
             />
           </div>
+          {showPassword ? (
+                    <FaEye className="h-5 w-5 text-gray-400 cursor-pointer m-1" onClick={togglePasswordVisibility} />
+                  ) : (
+                    <FaEyeSlash className="h-5 w-5 text-gray-400 cursor-pointer m-1" onClick={togglePasswordVisibility} />
+            )}
         </div>
 
         <div>
@@ -67,7 +108,7 @@ const Login = () => {
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Login
+             {loading?<Spinner />:"Signin"}
           </button>
         </div>
       </form>
